4c0ee81ee351617462317f87af935f03c721d1b9
Cody Brill <ncbrill@gmail.com>
2014-08-19 21:45:02 -0700

    Krio's Test

---
+++ Assets/Cody's Changes/WaterCollision.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterCollision : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision) {
+		if(collision.gameObject.tag == "watercollider") {
+			audio.Play ();
+		}
+	}
+}
